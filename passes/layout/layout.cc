2bb7e44735832f165c990d24cfa7e642946376eb
Ned Bingham <eab242@cornell.edu>
2020-11-13 23:57:05 -0500

    I figured out the ActPass infrastructure

--- passes/layout/layout.cc
+++ passes/layout/layout.cc
@@ -33,307 +33,179 @@
 #include <config.h>
 #include <stdint.h>
 
-act_dev_group::act_dev_group()
+act_coord::act_coord()
 {
 }
 
-act_dev_group::act_dev_group(unsigned int source, unsigned int drain)
+act_coord::act_coord(unsigned int x, unsigned int y)
 {
+	this->x = x;
+	this->y = y;
+}
+
+act_coord::~act_coord()
+{
+}
+
+net::net()
+{
+	node = NULL;
+	A_INIT(act_coords);
+}
+
+net::~net()
+{
+	A_FREE(act_coords);
+}
+
+act_dev::act_dev(unsigned int gate, unsigned int source, unsigned int drain, unsigned int bulk, unsigned int width, unsigned int length)
+{
+	this->gate = gate;
 	this->source = source;
 	this->drain = drain;
-}
-
-act_dev_group::~act_dev_group()
-{
-} 
-
-act_dev::act_dev()
-{
-}
-
-act_dev::act_dev(unsigned int type, unsigned int gate, act_dev_group_t group, act_size_spec_t *size)
-{
-	this->type = type;
-	this->gate = gate;
-	this->source = group.source;
-	this->drain = group.drain;
-	this->size = size;
+	this->bulk = bulk;
+	this->width = width;
+	this->length = length;
 }
 
 act_dev::~act_dev()
 {
 }
 
-act_cell::act_cell()
+layout_task::layout_task()
 {
+	A_INIT(pmos);
+	A_INIT(nmos);
 	A_INIT(nets);
-	A_INIT(devs);
 }
 
-act_cell::~act_cell()
+layout_task::~layout_task()
 {
+	A_FREE(pmos);
+	A_FREE(nmos);
+	A_FREE(nets);
 }
 
-unsigned int act_cell::netId(ActId *name)
+void ActLayoutPass::process_cell(Process *p)
 {
-	for (unsigned int i = 0; i < A_LEN(nets); i++)
-		if (nets[i] == name or (nets[i] != NULL and name->isEqual(nets[i])))
-			return i;
-	return A_LEN(nets);
-}
-
-void ActLayoutPass::extract_devices(act_cell_t *c, act_prs_expr_t *e, act_size_spec_t **sz, act_dev_group_t group, unsigned char type)
-{
-	if (e)
-	{
-		unsigned int node;
-		switch (e->type) {
-		case ACT_PRS_EXPR_AND:
-			A_NEW(c->nets, ActId*);
-			A_NEXT(c->nets) = NULL;
-			node = A_LEN(c->nets);
-			A_INC(c->nets);
-			extract_devices(c, e->u.e.l, sz, act_dev_group_t(group.source, node), type);
-			extract_devices(c, e->u.e.r, sz, act_dev_group(node, group.drain), type);
-			break;
-
-		case ACT_PRS_EXPR_OR:
-			extract_devices(c, e->u.e.l, sz, group, type);
-			extract_devices(c, e->u.e.r, sz, group, type);
-			break;
-
-		case ACT_PRS_EXPR_NOT:
-			extract_devices(c, e->u.e.l, sz, group, 1-type);
-			break;
-
-		case ACT_PRS_EXPR_LABEL:
-			break;
-
-		case ACT_PRS_EXPR_VAR:
-    if (e->u.v.sz) {
-      *sz = e->u.v.sz;
-    } else {
-      e->u.v.sz = *sz;
-    }
-		node = c->netId(e->u.v.id);
-		if (node >= A_LEN(c->nets)) {
-			A_NEW(c->nets, ActId*);
-			A_NEXT(c->nets) = e->u.v.id;
-			A_INC(c->nets);
-		}
-
-		A_NEW(c->devs, act_dev_t);
-		A_NEXT(c->devs) = act_dev_t(type, node, group, e->u.v.sz);
-		A_INC(c->devs);
-    break;
-    
-		case ACT_PRS_EXPR_ANDLOOP:
-		case ACT_PRS_EXPR_ORLOOP:
-			Assert (0, "Loops?!");
-			break;
-
-		case ACT_PRS_EXPR_TRUE:
-		case ACT_PRS_EXPR_FALSE:
-			break;
-		}
-	}
-}
-
-void ActLayoutPass::extract_stack(act_prs_lang_t *lang, act_cell_t *c)
-{
-	if (lang->type == ACT_PRS_RULE)
-	{
-		act_size_spec_t *sz = NULL;
-		act_dev_group group(lang->u.one.dir, c->netId(lang->u.one.id));
-		if (group.drain >= A_LEN(c->nets)) {
-			A_NEW(c->nets, ActId*);
-			A_NEXT(c->nets) = lang->u.one.id;
-			A_INC(c->nets);
-		}
-
-		extract_devices(c, lang->u.one.e, &sz, group);
-	}
-	else
-	{
-		fatal_error ("Cell `cell::%s': no fets/subckts/dup trees allowed", c->p->getName());
-	}
-}
-
-act_cell_t *ActLayoutPass::extract_cell(Process *p)
-{
-	/* cells in the "cells" namespace are very special.
-
-		 1. no aliases internally
-		 2. defcell has two ports: in[n], out[m]
-		 3. only prs for out[m]'s are present
-
-		 4. cell names are g<#> (for "gate")
-		 5. pass gate cells are t0,t1,n0,n1,p0,p1
-	*/
-	if (p->getName()[0] == 'g') {
-		/* ok, gate! */
-		if (p->getNumParams() != 0) {
-			fatal_error ("Unexpected cell `%s' in cell namespace (has parameters?)", p->getName());
-		}
-		if (p->getNumPorts() != 2) {
-			fatal_error ("Cell `cell::%s': More than two ports");
-		}
-		if ((strcmp (p->getPortName (0), "in") != 0) ||
-		    (strcmp (p->getPortName (1), "out") != 0)) {
-			fatal_error ("Cell `cell::%s': Ports should be in/out", p->getName());
-		}
-		InstType *in_t, *out_t;
-		in_t = p->getPortType (0);
-		out_t = p->getPortType (1);
-		
-		/* in_t must be a bool array or bool
- out_t must be a bool array or bool
-		*/
-		if (!TypeFactory::isBoolType (in_t) || !TypeFactory::isBoolType (out_t)) {
-			fatal_error ("Cell `cell::%s': Port base types must `bool'", p->getName());
-		}
-
-		/* sanity check prs */
-		act_prs *prs = p->getprs();
-		if (prs && prs->next) {
-			fatal_error ("Cell `cell::%s': More than one prs body", p->getName());
-		}
-
-		act_prs_lang_t *l;
-		Expr *treeval;
-		l = NULL;
-		treeval = NULL;
-		act_cell_t *c = NULL;
-		if (prs != NULL) {
-			c = new act_cell_t();
-			c->p = p;
-
-			A_NEW(c->nets, ActId*);
-			A_NEXT(c->nets) = prs->gnd;
-			A_INC(c->nets);
-			A_NEW(c->nets, ActId*);
-			A_NEXT(c->nets) = prs->vdd;
-			A_INC(c->nets);
-			
-			l = prs->p;
-			if (l && l->type == ACT_PRS_TREE) {
-				if (l->next) {
-					fatal_error ("Cell `cell::%s': only one tree permitted", p->getName());
-				}
-				l = l->u.l.p;
-			}
-			while (l) {
-				Assert (l->type != ACT_PRS_LOOP, "Expanded?!");
-				if (l->type == ACT_PRS_GATE || l->type == ACT_PRS_SUBCKT ||
-						l->type == ACT_PRS_TREE) {
-					fatal_error ("Cell `cell::%s': no fets/subckts/dup trees allowed", p->getName());
-				}
-				else if (l->type == ACT_PRS_RULE) {
-					extract_stack(l, c);
-				}
-				l = l->next;
-			}
-			l = prs->p;
-			if (l && l->type == ACT_PRS_TREE) {
-				treeval = l->u.l.lo;
-				l = l->u.l.p;
-				extract_stack(l, c);
-			}
-
-			printf ("CELL: %s\n", p->getName());
-			for (int i = 0; i < A_LEN(c->devs); i++) {
-				printf("%s g:%d s:%d d:%d", c->devs[i].type == ACT_NMOS ? "nmos" : "pmos", c->devs[i].gate, c->devs[i].source, c->devs[i].drain);
-				if (c->devs[i].size == NULL) {
-					printf(" w,l:null");
-				} else {
-					if (c->devs[i].size->w == NULL or c->devs[i].size->w->type != E_INT) {
-						printf(" w:<e>");
-					} else {
-						printf(" w:%u", c->devs[i].size->w->u.v);
-					}
-					
-					if (c->devs[i].size->l == NULL or c->devs[i].size->l->type != E_INT) {
-						printf(" l:<e>");
-					} else {
-						printf(" l:%u", c->devs[i].size->l->u.v);
-					}
-				}
-				printf("\n");
-
-			}
-		}
-
-		return c;
+	netlist_t *n = np->getNL(p);
+	if (n == NULL) {
+		fatal_error ("Process has not been netlisted.");
 	}
 	
-	return NULL;
-	// cells are small, so we can just brute-force the transistor stack ordering. Then we can use Channel Routing to route all of the nets.
-}
+	layout_task task;
 
-void ActLayoutPass::collect_cells(Process *p)
-{
-  ActNamespace *cell_ns = a->findNamespace ("cell");
-	if (!cell_ns) {
-		return;
+	int max_net_id = -1;
+	for (node_t *x = n->hd; x; x = x->next) {
+		if (x->i > max_net_id) {
+			max_net_id = x->i;
+		}
+	}
+	max_net_id += 1;
+
+	A_NEWP(task.nets,net_t,max_net_id);
+
+	printf("max_net_id: %d\n", max_net_id);
+
+	for (node_t *x = n->hd; x; x = x->next) {
+		printf("node: %d\n", x->i);
+		task.nets[x->i].node = x;
+
+		listitem_t *li;
+
+		for (li = list_first(x->e); li; li = list_next(li)) {
+			edge_t *e = (edge_t *)list_value(li);
+			
+			int len_repeat, width_repeat;
+      int width_last;
+      int il, iw;
+      int w, l;
+      int fold;
+
+			
+			if (e->visited || e->pruned)
+				continue;
+			
+			e->visited = 1;
+	
+			w = e->w;
+			l = e->l;
+
+			/* discretize lengths */
+			len_repeat = e->nlen;
+			if (discrete_len > 0) {
+				l = discrete_len;
+			}
+
+			if (e->type == EDGE_NFET) {
+				fold = n_fold;
+			} else {
+				Assert (e->type == EDGE_PFET, "Hmm");
+				fold = p_fold;
+			}
+
+			width_repeat = e->nfolds;
+
+			for (int il = 0; il < len_repeat; il++) {
+				for (int iw = 0; iw < width_repeat; iw++) {
+					if (width_repeat > 1) {
+						w = EDGE_WIDTH (e, iw);
+					} else {
+						w = e->w;
+					}
+
+					if (e->type == 0) {
+						A_NEW(task.nmos, act_dev_t);
+						A_NEXT(task.nmos) = act_dev_t(e->g->i, e->a->i, e->b->i, e->bulk->i, w, l);
+						A_INC(task.nmos);
+					} else {
+						A_NEW(task.pmos, act_dev_t);
+						A_NEXT(task.pmos) = act_dev_t(e->g->i, e->a->i, e->b->i, e->bulk->i, w, l);
+						A_INC(task.pmos);
+					}
+				}
+			}
+		}
 	}
 
-	ActTypeiter it(cell_ns);
-
-	A_DECL (const char *, xcell);
-  A_INIT (xcell);
-
-	for (it = it.begin(); it != it.end(); it++) {
-    Type *u = (*it);
-    Process *p = dynamic_cast<Process *>(u);
-    if (!p) continue;
-
-    if (!p->isCell()) {
-      continue;
+	for (node_t *x = n->hd; x; x = x->next) {
+    listitem_t *li;
+    for (li = list_first (x->e); li; li = list_next (li)) {
+      edge_t *e = (edge_t *)list_value (li);
+      e->visited = 0;
     }
+  }
 
-		if ((!p->isExpanded()) && (p->getNumParams() == 0)) {
-      A_NEW (xcell, const char *);
-      A_NEXT (xcell) = p->getName();
-      A_INC (xcell);
-    }
+	char buf[1000];
+	for (int i = 0; i < A_LEN(task.nets); i++) {
+		if (task.nets[i].node != NULL and task.nets[i].node->v != NULL) {
+			ActId *id = task.nets[i].node->v->v->id->toid();
+			id->sPrint(buf, 1000);
+			printf("%d: \"%s\"\n", i, buf);
+		} else {
+			printf("%d: \"\"\n", i);
+		}
 	}
 
-	for (int i=0; i < A_LEN (xcell); i++) {
-    char buf[10240];
-    sprintf (buf, "%s<>", xcell[i]);
-    Type *u = cell_ns->findType (xcell[i]);
-    Assert (u, "What?");
-    Process *p = dynamic_cast<Process *>(u);
-    Assert (p, "What?");
-    if (!cell_ns->findType (buf)) {
-      /* create expanded version */
-      p->Expand (cell_ns, cell_ns->CurScope(), 0, NULL);
-    }
-  }
-  A_FREE (xcell);
+	for (int i = 0; i < A_LEN(task.pmos); i++) {
+		printf("pmos g:%d s:%d d:%d b:%d w:%d l:%d\n", task.pmos[i].gate, task.pmos[i].source, task.pmos[i].drain, task.pmos[i].bulk, task.pmos[i].width, task.pmos[i].length);
+	}
 
-	for (it = it.begin(); it != it.end(); it++) {
-    Type *u = *it;
-    Process *p = dynamic_cast <Process *>(u);
-    if (!p) {
-      continue;
-    }
-    if (!p->isCell()) {
-      continue;
-    }
-    if (!p->isExpanded()) {
-      continue;
-    }
+	for (int i = 0; i < A_LEN(task.nmos); i++) {
+		printf("nmos g:%d s:%d d:%d b:%d w:%d l:%d\n", task.nmos[i].gate, task.nmos[i].source, task.nmos[i].drain, task.nmos[i].bulk, task.nmos[i].width, task.nmos[i].length);
+	}
 
-		act_cell_t *c = extract_cell(p);
-		if (c != NULL)
-			delete c;
-  }
 }
 
 void *ActLayoutPass::local_op (Process *p, int mode)
 {
-	collect_cells(p);
+	if (p and p->isCell()) {
+		if (p->isExpanded()) {
+			process_cell(p);
+		} else {
+			fatal_error ("Process has not been expanded.");
+		}
+	}
 	return NULL;
 }
 
@@ -341,21 +213,20 @@ void ActLayoutPass::free_local (void *v)
 {
 }
 
-int ActLayoutPass::run (Process *p)
-{
-  int ret = ActPass::run (p);
-
-  return ret;
-}
-
-
-void ActLayoutPass::Print (FILE *fp)
-{
-}
-
-
 ActLayoutPass::ActLayoutPass (Act *a) : ActPass (a, "cells2layout")
 {
+	np = (ActNetlistPass*)a->pass_find("prs2net");
+  if (!np) {
+    np = new ActNetlistPass (a);
+  }
+	Assert(np, "What?");
+  AddDependency ("prs2net");
+
+  n_fold = config_get_int ("net.fold_nfet_width");
+  p_fold = config_get_int ("net.fold_pfet_width");
+  discrete_len = config_get_int ("net.discrete_length");
+
+  lambda = config_get_real ("net.lambda");
 }
 
 ActLayoutPass::~ActLayoutPass ()
--- transform/cells2layout/main.cc
+++ transform/cells2layout/main.cc
@@ -25,43 +25,170 @@
 #include <unistd.h>
 #include <string.h>
 #include <act/act.h>
-#include <act/passes.h>
+#include <act/passes/netlist.h>
+#include <act/passes/cells.h>
 #include <act/passes/layout.h>
-#include "config.h"
+#include <config.h>
 
+/*
+  Check for trailing .extension
+*/
+static int has_trailing_extension (const char *s, const char *ext)
+{
+  int l_s, l_ext;
+	
+  l_s = strlen (s);
+  l_ext = strlen (ext);
+	
+  if (l_s < l_ext + 2) return 0;
+	
+  if (strcmp (s + l_s - l_ext, ext) == 0) {
+    if (s[l_s-l_ext-1] == '.')
+      return 1;
+  }
+  return 0;
+}
 
 static void usage (char *name)
 {
-  fprintf (stderr, "Usage: %s [act-options] <actfile> <celldir>\n", name);
+  fprintf (stderr, "Usage: %s [act-options] [-dltBR] [-o layoutdir] <act>\n", name);
+  fprintf (stderr, " -c <cells> Cell file name\n");
+  fprintf (stderr, " -p <proc> Emit process <proc>\n");
+  fprintf (stderr, " -o <file> Save result to <file> rather than stdout\n");
+  fprintf (stderr, " -S        Enable shared long-channel devices in staticizers\n");
   exit (1);
 }
 
+static int enable_shared_stat = 0;
+static char *cell_file;
+static char *layout_dir;
+
+/*
+  Initialize globals from the configuration file.
+*/
+static char *initialize_parameters (int *argc, char ***argv)
+{
+  char *proc_name;
+  char *act_cmdline;
+  int ch;
+
+	proc_name = NULL;
+  cell_file = NULL;
+  layout_dir = NULL;
+
+  config_set_default_string ("net.global_vdd", "Vdd");
+  config_set_default_string ("net.global_gnd", "GND");
+  config_set_default_string ("net.local_vdd", "VddN");
+  config_set_default_string ("net.local_gnd", "GNDN");
+
+  Act::Init (argc, argv);
+
+  while ((ch = getopt (*argc, *argv, "SBdtp:o:lc:")) != -1) {
+    switch (ch) {
+    case 'S':
+      enable_shared_stat = 1;
+      break;
+ 
+		case 'p':
+      if (proc_name) {
+	FREE (proc_name);
+      }
+      proc_name = Strdup (optarg);
+      break;
+     
+    case 'o':
+			if (layout_dir) {
+				FREE(layout_dir);
+			}
+			layout_dir = Strdup(optarg);
+      break;
+
+    case 'c':
+      if (cell_file) {
+	FREE (cell_file);
+      }
+      cell_file = Strdup (optarg);
+      break;
+    case '?':
+      fprintf (stderr, "Unknown option.\n");
+      usage ((*argv)[0]);
+      break;
+      
+    default:
+      fatal_error ("shouldn't be here");
+      break;
+    }
+  }
+
+  /* optind points to what is left */
+  /* expect 1 argument left */
+  if (optind != *argc - 1) {
+    fprintf (stderr, "Missing act file name.\n");
+    usage ((*argv)[0]);
+  }
+
+	if (!proc_name) {
+    fprintf (stderr, "Missing process name.\n");
+    usage ((*argv)[0]);
+  }
+
+  *argc = 2;
+  (*argv)[1] = (*argv)[optind];
+  (*argv)[2] = NULL;
+
+	return proc_name;
+}
 
 int main (int argc, char **argv)
 {
   Act *a;
-  char *proc;
-  FILE *fp;
+	char *proc;
 
-  Act::Init (&argc, &argv);
+  proc = initialize_parameters (&argc, &argv);
 
-  if (argc != 3) {
-    usage (argv[0]);
+  if (argc != 2) {
+    fatal_error ("Something strange happened!");
+  }
+ 	if (proc == NULL) {
+    fatal_error ("Missing process name!");
+  }
+ 
+  a = new Act (argv[1]);
+  
+  if (cell_file) {
+    a->Merge (cell_file);
+  }
+  
+  a->Expand ();
+
+  if (cell_file) {
+    ActCellPass *cp = new ActCellPass (a);
+    cp->run();
+
+    FILE *oc = fopen (cell_file, "w");
+    if (!oc) {
+      fatal_error ("Could not write cell file");
+    }
+    cp->Print (oc);
+    fclose (oc);
+  }
+  
+  if (config_exists ("net.mangle_chars")) {
+    a->mangle (config_get_string ("net.mangle_chars"));
   }
 
-  a = new Act (argv[1]);
-  a->Expand ();
-  /* for each expanded ACT process, read in cells */
+	Process *p = a->findProcess (proc);
 
-  /* for each ACT process, find production rules and try and match
-     to cells */
+  if (!p) {
+    fatal_error ("Could not find process `%s' in file `%s'", proc, argv[1]);
+  }
 
-  ActLayoutPass *cp = new ActLayoutPass (a);
-  cp->run();
+  if (!p->isExpanded()) {
+    fatal_error ("Process `%s' is not expanded.", proc);
+  }
 
-  //cp->Print (stdout);
-
-  //a->Print (stdout);
+	ActLayoutPass *lp = new ActLayoutPass(a);
+	lp->run(p);
 
   return 0;
 }
